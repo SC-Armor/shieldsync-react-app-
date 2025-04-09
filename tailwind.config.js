1dd2c65847b0a47f87d37d1466eff43e567285d3
SC-Armor <theforemans2002@gmail.com>
2025-04-09 11:32:11 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
